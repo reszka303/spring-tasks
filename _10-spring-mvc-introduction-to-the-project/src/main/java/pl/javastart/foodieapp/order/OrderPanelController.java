8c7676e86061d6985fbf9173051e46c2ece3c5df
reszka303 <reszka303@op.pl>
2022-02-05 02:09:00 +0100

    31st commit: Spring MVC: Ordering: new features: added a panel in which to be displayed all orders in the form of the readable list (update)

--- _10-spring-mvc-introduction-to-the-project/src/main/java/pl/javastart/foodieapp/order/OrderPanelController.java
+++ _10-spring-mvc-introduction-to-the-project/src/main/java/pl/javastart/foodieapp/order/OrderPanelController.java
@@ -3,9 +3,13 @@ package pl.javastart.foodieapp.order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.javastart.foodieapp.item.Item;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class OrderPanelController {
@@ -17,14 +21,39 @@ public class OrderPanelController {
     }
 
     @GetMapping("/panel/zamowienia")
-    public String getOrders(@RequestParam(required = false) OrderStatus status, Model model) {
+    public String getOrders(@RequestParam(required = false) OrderStatus status,
+                            Model model) {
         List<Order> orders;
         if(status == null)
             orders = orderRepository.findAll();
-         else
+        else
             orders = orderRepository.findAllByStatus(status);
-            model.addAttribute("orders", orders);
+        model.addAttribute("orders", orders);
         return "panel/orders";
+    }
+
+    @GetMapping("/panel/zamowienie/{id}")
+    public String singleOrder(@PathVariable Long id, Model model) {
+        Optional<Order> order = orderRepository.findById(id);
+        return order.map(o -> singleOrderPanel(o, model))
+                .orElse("redirect:/");
+    }
+
+    @PostMapping("/panel/zamowienie/{id}")
+    public String changeOrderStatus(@PathVariable Long id, Model model) {
+        Optional<Order> order = orderRepository.findById(id);
+        order.ifPresent(o -> {
+            o.setStatus(OrderStatus.nextStatus(o.getStatus()));
+            orderRepository.save(o);
+        });
+        return order.map(o -> singleOrderPanel(o, model))
+                .orElse("redirect:/");
+    }
+
+    private String singleOrderPanel(Order order, Model model) {
+        model.addAttribute("order", order);
+        model.addAttribute("sum", order.getItems().stream().mapToDouble(Item::getPrice).sum());
+        return "panel/order";
     }
 
 }
--- _10-spring-mvc-introduction-to-the-project/src/main/java/pl/javastart/foodieapp/order/OrderStatus.java
+++ _10-spring-mvc-introduction-to-the-project/src/main/java/pl/javastart/foodieapp/order/OrderStatus.java
@@ -3,5 +3,15 @@ package pl.javastart.foodieapp.order;
 public enum OrderStatus {
     NEW,
     IN_PROGRESS,
-    COMPLETE
+    COMPLETE;
+
+    static OrderStatus nextStatus(OrderStatus status) {
+        if (status == NEW)
+            return IN_PROGRESS;
+        else if (status == IN_PROGRESS)
+            return COMPLETE;
+        else
+            throw new IllegalArgumentException("There is no next status for provided value");
+    }
+
 }
