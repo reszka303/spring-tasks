6ec0bdd31edc5ed2122f61bfb7234c8bc7bfacc6
reszka303 <reszka303@op.pl>
2022-03-06 20:50:08 +0100

    32nd commit: Spring MVC REST: initial commit

---
+++ _11-equipy/src/main/java/com/example/equipy/EquipyApplication.java
@@ -0,0 +1,13 @@
+package com.example.equipy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipyApplication.class, args);
+    }
+
+}
